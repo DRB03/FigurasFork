5fff36d0ad7b9604003dc49a6d43c89ce48e0a8f
DRB03 <126363236+DRB03@users.noreply.github.com>
2023-04-26 17:20:38 +0000

    Mi juego

--- sketch.js
+++ sketch.js
@@ -1,102 +1,108 @@
-class Figura {
-   constructor(x, y, alto, ancho, vx, vy) {
-    this.posicion = createVector(x,y);
-    this.alto = alto;
-    this.ancho = ancho;
-    this.fillred = 255;
-    this.fillgreen = 87;
-    this.fillblue = 57;
-    this.velocidad = createVector(vx,vy);
+let player;
+let objects = [];//almacena la posicion de los objetos
+let obstacles = [];//almacena la posicion de los obtaculos
+let score = 0;
+
+function setup() //creo el canvas y el jugador
+{
+  createCanvas(600, 600);
+  player = createVector(width/2, height/2);
+}
+
+function draw()
+{
+  background(100);
+  movePlayer();//movimiento del jugador
+  drawPlayer();//la apariencia 
+  drawObjects();//dibuja los objetos
+  drawObstacles();//dibuja los obtaculos
+  checkCollisions();//verifica una colision y modifica el puntaje 
+  drawScore();//muestra la puntuacion
+}
+
+function movePlayer()//permite que el jugador se pueda mover con el teclado
+{
+  
+  if (keyIsDown(LEFT_ARROW)) {
+    player.x -= 5;
   }
-  update()
+  if (keyIsDown(RIGHT_ARROW)) {
+    player.x += 5;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    player.y -= 5;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    player.y += 5;
+  }
+}
+
+function drawPlayer()//creo al jugador
+{
+  fill(0, 255, 0);
+  ellipse(player.x, player.y, 20, 20);
+}
+
+function drawObjects()//creo los objetos que suman puntos
+{
+  fill(255, 0, 0);
+  for (let i = 0; i < objects.length; i++) {
+    ellipse(objects[i].x, objects[i].y, 10, 10);
+  }
+}
+
+function drawObstacles()//creo obstaculos que restan puntos
+{
+  fill(0, 0, 255);
+  for (let i = 0; i < obstacles.length; i++) {
+    rect(obstacles[i].x, obstacles[i].y, 20, 20);
+  }
+}
+
+function checkCollisions()
+//verifico si choco con un objeto u obstaculo y sumo o resto puntos
+{
+  for (let i = 0; i < objects.length; i++) 
   {
-      if (this.posicion.x + this.ancho >= 400)
-        {  
-          let valor = random(3); 
-          this.velocidad.x = this.velocidad.x * -valor;
-         this.velocidad.y = this.velocidad.y * -valor;
-        }
-      this.posicion.add(this.velocidad);
+    let d = dist(player.x, player.y, objects[i].x, objects[i].y);
+    if (d < 15) {
+      objects.splice(i, 1);
+      score += 10;
+    }
   }
   
-}
-
-class Rectangulo extends Figura{
-  constructor(x, y, alto, ancho, vx, vy) {
-      super(x, y, alto, ancho, vx, vy);
-  }
-  
-draw()
+  for (let i = 0; i < obstacles.length; i++) 
   {
-fill(this.fillred,this.fillgreen,this.fillblue);
-rect(this.posicion.x,this.posicion.y,this.alto,this.ancho);
+    let d = dist(player.x, player.y, obstacles[i].x, obstacles[i].y);
+    if (d < 20) {
+      obstacles.splice(i, 1);
+      score -= 5;
+    }
   }
 }
 
-class Elipse extends Figura{
-  constructor(x, y, alto, ancho, vx, vy) {
-      super(x, y, alto, ancho, vx, vy);
-  }
-  
-draw()
-  {
-fill(this.fillred,this.fillgreen,this.fillblue);
-ellipse(this.posicion.x,this.posicion.y,this.alto,this.ancho);
-  }
+function drawScore() //muestra la puntuacion actual del jugador
+{
+  textSize(20);
+  textAlign(RIGHT);
+  fill(0);
+  text("Puntaje(: " + score, width - 20, 30);
 }
 
-var figuras = [];
-var dibujando = 'circulo';
-var btnCirculo = null;
-var btnRectangulo = null;
-
-
-function mouseClicked() {
-  // Se crea un objeto según la opción actual
-if (mouseY > 25)
-  {
-  if (dibujando == 'circulo')
-    figuras.push(new Elipse(mouseX,mouseY,20,20,3,1));
-  else if (dibujando == 'rectangulo')
-    figuras.push(new Rectangulo(mouseX,mouseY,20,20,2,1));
-  }
-
-  return false;
+function spawnObject()
+//creo objetos en posiciones random cada segundo
+{
+  let object = createVector(random(width), random(height));
+  objects.push(object);
 }
 
-function setup() {
-  createCanvas(400, 400);
-  
-  btnCirculo = createButton('Circulo');
-  btnCirculo.position(0, 0);
-  btnCirculo.mousePressed(changeCirculo);
-  btnCirculo.style( 'background-color','#cccccc');
-  
-  btnRectangulo = createButton('Rectangulo');
-  btnRectangulo.position(75, 0);
-  btnRectangulo.mousePressed(changeRectangulo);
+setInterval(spawnObject, 800);
+
+function spawnObstacle()
+//creo obtaculos en posicion random cada segundo y medio
+{
+  let obstacle = createVector(random(width), random(height));
+  obstacles.push(obstacle);
 }
 
-function changeCirculo()
-   {
-     btnCirculo.style( 'background-color','#cccccc');
-     btnRectangulo.style( 'background-color','#f0f0f0');
-     dibujando = 'circulo';
-   }
-function changeRectangulo()
-   {
-     btnRectangulo.style( 'background-color','#cccccc');
-     btnCirculo.style( 'background-color','#f0f0f0');
-     dibujando = 'rectangulo';
-   }
-
- 
-
-function draw() {
-  background(220);
-  figuras.forEach((fig) => 
-   {
-    fig.draw();
-    fig.update();
-   });
-}
+setInterval(spawnObstacle, 1000);
