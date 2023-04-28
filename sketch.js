77a06377c4f30926752e972957e63eeb94938fb1
DRB03 <126363236+DRB03@users.noreply.github.com>
2023-04-28 00:53:42 +0000

    MI JUEGOO:)

--- sketch.js
+++ sketch.js
@@ -2,27 +2,34 @@ let shapes = [];
 let isCircle = true;
 let player;
 
-class Shape {
-  constructor(x, y, size, speed) {
+class Shape
+//Clase base que define las propiedades y los metodos a todas las formas 
+ {
+  constructor(x, y, size, speed) 
+  {
     this.x = x;
     this.y = y;
     this.size = size;
     this.speed = speed;
   }
 
-  move() {
+  move() 
+  {
     this.x += this.speed;
-    if (this.x > width + this.size) {
+    if (this.x > width + this.size) 
+    {
       this.x = -this.size;
     }
   }
 
-  display() {
+  display() 
+  {
     // La clase Shape no tiene una implementación específica de display
     // En cambio, cada subclase de Shape implementará su propia versión de display
   }
 
-  collide(playerX, playerY, playerSize) {
+  collide(playerX, playerY, playerSize) 
+  {
     let d = dist(this.x, this.y, playerX, playerY);
     if (d < this.size/2 + playerSize/2) {
       return true;
@@ -32,37 +39,50 @@ class Shape {
   }
 }
 
-class Circle extends Shape {
-  constructor(x, y, size, speed) {
+class Circle extends Shape
+//Hereda de la clase Shape y tambien tiene sus propios metodos 
+ {
+  constructor(x, y, size, speed) 
+  {
     super(x, y, size, speed);
   }
 
-  display() {
+  display() //hereda de la clase shape pero la sobreescribo para dar una vista unica a cada forma
+  {
     fill(255, 0, 0);
     ellipse(this.x, this.y, this.size);
   }
 }
 
-class Square extends Shape {
-  constructor(x, y, size, speed) {
+class Square extends Shape 
+//Hereda de la clase Shape y tambien tiene sus propios metodos
+{
+  constructor(x, y, size, speed) 
+  {
     super(x, y, size, speed);
   }
 
-  display() {
+  display()//hereda de la clase shape pero la sobreescribo para dar una vista unica a cada forma 
+  {
     fill(0, 0, 255);
     rect(this.x, this.y, this.size, this.size);
   }
 }
 
-class Player {
-  constructor(x, y, size, speed) {
+class Player
+//clase para el jugador
+ {
+  constructor(x, y, size, speed) 
+  {
     this.x = x;
     this.y = y;
     this.size = size;
     this.speed = speed;
   }
 
-  move() {
+  move() 
+  //permite mover al jugador con los botones del teclado
+  {
     if (keyIsDown(LEFT_ARROW)) {
       this.x -= this.speed;
     }
@@ -80,20 +100,25 @@ class Player {
     this.y = constrain(this.y, this.size/2, height - this.size/2);
   }
 
-  display() {
+  display()
+  {
     fill(0, 255, 0);
     ellipse(this.x, this.y, this.size);
   }
 
-  collide(shape) {
+  collide(shape) 
+  {
     return shape.collide(this.x, this.y, this.size);
   }
 }
 
-function setup() {
+function setup() 
+//incia el canvas y creo los circulos y cuadrados
+{
   createCanvas(500, 500);
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) 
+  {
     if (isCircle) {
       shapes.push(new Circle(random(width), random(height), 30, random(1, 2)));
     } else {
@@ -104,13 +129,16 @@ function setup() {
   player = new Player(width/2, height/2, 30, 5);
 }
 
-function draw() {
+function draw()
+//actualiza la posicion de las formas y dibuja el canvas 
+ {
   background(100);
 
   player.move();
   player.display();
 
-  for (let i = 0; i < shapes.length; i++) {
+  for (let i = 0; i < shapes.length; i++) 
+  {
     shapes[i].move();
     shapes[i].display();
 
@@ -123,7 +151,9 @@ function draw() {
   }
 }
 
-function keyPressed() {
+function keyPressed()
+//Al presionar la barra de espacio se cambian las figuras de cuadrados a circulos o viceversa
+ {
   if (key == ' ') {
     isCircle = !isCircle; // Invertir el valor de isCircle al presionar la barra espaciadora
     shapes = []; // Limpiar el arreglo de formas existentes
